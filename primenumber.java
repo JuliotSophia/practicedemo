dcff2d2150f92634a3ebdfb7bb3ffe23dcdc8409
Ignatius Vijay <iggivijay@gmail.com>
2023-10-30 14:42:00 +0530

    prime number

---
+++ primenumber.java
@@ -0,0 +1,19 @@
+public class primenumber {
+  public static void main(String args[]) {
+      int n = 5;
+      int s;
+      int t = 0;
+      for (int i = 2; i <= n; i++) {
+          s = n % i;
+          if (s == 0) {
+             t++;;
+          }
+      }
+          if (t== 0) {
+              System.out.println(n + "" + "is a prime number");
+          } else {
+              System.out.println(n + "" + "is a notprime number");
+          }
+      }
+  }
+
